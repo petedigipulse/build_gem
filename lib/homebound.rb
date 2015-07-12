9061c9f2742f9f42c0dc0775b1290c401d88c6d2
petedigipulse <peter@digitalpulse.co.nz>
2015-07-13 11:55:32 +1200

    Successfully tested gem install from commandline

--- lib/homebound.rb
+++ lib/homebound.rb
@@ -1,6 +1,6 @@
 class Homebound
 	def self.hi
-		puts "Hello HomeBound Users, Here is your data. THis Gem is now available on RubyGems.org"
+		puts "Hello HomeBound Users, Here is your data. This Gem is now available on RubyGems.org. This gem can be installed via commnadline"
 	end
 end
 
