337976f1edf5bf000eacc21ac855bb04e0650423
petedigipulse <peter@digitalpulse.co.nz>
2015-07-13 11:48:44 +1200

    Pushed gem to RubyGems.org, is listed and searchable on site

--- lib/homebound.rb
+++ lib/homebound.rb
@@ -1,6 +1,6 @@
 class Homebound
 	def self.hi
-		puts "Hello HomeBound Users, Here is your data"
+		puts "Hello HomeBound Users, Here is your data. THis Gem is now available on RubyGems.org"
 	end
 end
 
